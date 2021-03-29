fa51d28bc5447573c2dcd69f7449d96e324ede37
jakkwi3 <kubakwiatek0@gmail.com>
2021-03-29 19:00:39 +0200

    Rozwiązanie zad 1 i 2

--- Kwiatek.R
+++ Kwiatek.R
@@ -5,3 +5,12 @@ t2 = "TERYT 22; pomorskie;   Gdańsk; 12 C ; B"
 str_sub(t1, start = -5, end = -4)
 str_sub(t2, start = -5, end = -4)
 # zadanie 2
+horoskop = function(imie, miesiac){
+  if(miesiac %% 2 == 0){
+    paste("Osoba o imieniu", imie, "będzie miała jutro szczęście")
+    } else {
+    paste("Osoba o imieniu", imie, "będzie miała jutro nieszczęście")
+    }
+}
+horoskop("Jan", 10)
+
