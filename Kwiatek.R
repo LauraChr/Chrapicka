aea39f2dce5226fb91ab604a52fda3b9b9e12943
jakkwi3 <kubakwiatek0@gmail.com>
2021-03-29 18:38:03 +0200

    Rozwiązanie 1

---
+++ Kwiatek.R
@@ -0,0 +1,7 @@
+# zadanie 1
+library(stringr)
+t1 = "TERYT 18; podkarpackie; Rzeszów; 0.2 He; A"
+t2 = "TERYT 22; pomorskie;   Gdańsk; 12 C ; B"
+str_sub(t1, start = -5, end = -4)
+str_sub(t2, start = -5, end = -4)
+# zadanie 2
